b0c0306ca36c30077a52233cf496bd2714516d21
Dell <Dell@192.168.0.8>
2020-06-14 21:33:56 +0530

    This is tenth problem in loop topic

---
+++ mileStone1/src/com/wipro/languagebasics/tenthLoop.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+public class tenthLoop {
+	public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(+i);
+        }
+	
+}
+}	
