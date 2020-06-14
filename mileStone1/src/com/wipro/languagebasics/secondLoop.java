a942dad4b442f7be731a7cd4fe9566d8caca57e7
Dell <Dell@192.168.0.8>
2020-06-14 20:23:18 +0530

    This is second problem in command loop topic

---
+++ mileStone1/src/com/wipro/languagebasics/secondLoop.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+import java.util.Scanner;
+public class secondLoop {
+	public static void main(String[] args) {
+	    Scanner myObj = new Scanner(System.in);
+	     int i = myObj.nextInt();
+	     if(i%2==0)
+	      System.out.println("The given number is a even Number");
+	     else
+	      System.out.println("The given number is a odd Number");
+	}
+}
