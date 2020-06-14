858a3bf1809f186dcd629b0b11f54e0c1efdc343
Dell <Dell@192.168.0.8>
2020-06-14 20:13:09 +0530

    This is first problem in command line topic

---
+++ mileStone1/src/com/wipro/languagebasics/firstCommandLine.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics;
+
+public class firstCommandLine {
+	public static void main(String[] args) 
+	{
+		System.out.println(args[0] +" Technologies "+ args[1]);
+		System.out.println(args[2] +" Technologies "+ args[3]);
+	}
+}
