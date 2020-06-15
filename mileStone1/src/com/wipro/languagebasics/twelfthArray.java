8ca67aa11107fed1b8fca655396e033fab668b97
Dell <Dell@192.168.0.8>
2020-06-15 09:22:45 +0530

    This is twelfth problem in Array topic

---
+++ mileStone1/src/com/wipro/languagebasics/twelfthArray.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class twelfthArray {
+	public static void main(String[] args)
+	 {
+	  int [] a=new int[] {1,2,3};
+	  int [] b= new int[] {4,5,6};
+	  System.out.println("{"+a[(a.length-1)/2]+","+b[(b.length-1)/2]+"}");
+	 }
+}
