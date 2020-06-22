fefdf39e99acfc37d1a6a0af6ca78be552ff1344
Dell <Dell@DESKTOP-AV5HPLJ>
2020-06-22 09:45:32 +0530

    This is second problem in OOPS

---
+++ mileStone1/src/com/wipro/oops/secondProgram.java
@@ -0,0 +1,20 @@
+package com.wipro.oops;
+
+public class secondProgram {
+	public static void main(String[] args) {
+		System.out.println(Calculator.powerDouble(13.89,4));
+		System.out.println(Calculator.powerInt(16,8));	
+	}
+}
+class Calculator
+{
+	static double powerInt(int num1,int num2)
+	{
+		return Math.pow(num1, num2);
+	}
+	static double powerDouble(double num1,int num2)
+	{
+		return Math.pow(num1, num2);
+	}
+}
+
