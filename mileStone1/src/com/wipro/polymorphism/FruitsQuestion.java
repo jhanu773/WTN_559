2016981a659ce9c87f71ae898da22c26156b1a39
Dell <Dell@192.168.0.5>
2020-06-24 20:57:39 +0530

    This is first problem in polymorphism

---
+++ mileStone1/src/com/wipro/polymorphism/FruitsQuestion.java
@@ -0,0 +1,35 @@
+package com.wipro.polymorphism;
+
+public class FruitsQuestion {
+	public static void main(String[] args) {
+		new Fruit().eat();
+		new Apple().eat();
+		new Orange().eat();
+	}
+}
+class Fruit {
+	 String name;
+	 String taste;
+	 int size;
+	
+	public Fruit() {
+		name = "Apple";
+		taste = "sweet";
+		size = 10;
+	}
+	
+	public void eat () {
+		System.out.println(name + " tastes so " + taste);
+	}
+}
+class Apple extends Fruit {
+	public void eat() {
+		System.out.println("It tastes like apple");
+	}
+}
+class Orange extends Fruit {
+	public void eat() {
+		System.out.println("It tastes like Orange");
+	}
+}
+
